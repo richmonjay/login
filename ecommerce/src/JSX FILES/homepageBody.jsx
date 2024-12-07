660c74f6b734dc23abcb8eecc2b2df6a118099e1
Jay <richmonjay.francisco@tup.edu.ph>
2024-12-08 01:56:42 +0800

    first commit

---
+++ ecommerce/src/JSX FILES/homepageBody.jsx
@@ -0,0 +1,27 @@
+import PatekBest from '../image/patekBest.png';
+
+function HomepageBody() {
+    return(
+        <div className="homepageBody-container">
+            <div className="leftHomepageBody">
+                <div className='watchLimit'>
+                    <button className='limitedTo'>Limited to 50 pieces</button>
+                </div>
+                <div className='watchName'>
+                    <p className='refNum'>5066A-001</p>
+                    <p className='brandName'>PATEK PHILIPPE AQUANAUT</p>
+                </div>
+                <div className='watchPrices'>
+                    <p className='watchPrice'>$40,578</p>
+                    <button className='buyButton'>BUY NOW</button>
+                </div>
+            </div>
+            <div className="middleHomepageBody">
+                <img src={PatekBest} alt="Patek Best Seller" className='patekBest' />
+            </div>
+            <div className="rightHomepageBody"></div>
+        </div>
+    )
+}
+
+export default HomepageBody;
